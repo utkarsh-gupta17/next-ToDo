e5a64df640d8889b366f190c588ce8db46924b81
utkarsh-gupta17 <211220061@gmail.com>
2024-01-03 19:43:06 +0530

    completed all routes

---
+++ src/app/api/users/[userId]/tasks/route.js
@@ -0,0 +1,20 @@
+import { Task } from "@/models/task";
+import { NextResponse } from "next/server";
+
+export async function GET(request,{params}){
+  const id = params.userId;
+  try {
+    const allTasks = await Task.find({
+      userId:id
+    });
+    console.log("successfully retrieved all tasks for the particular user");
+    return NextResponse.json(allTasks); 
+  } catch (error) {
+    console.log("error in fetching all tasks for a particular user");
+    console.log(error);
+    return NextResponse.json({
+      message:"failed to retrieve tasks for the user",
+      success:false
+    })
+  }
+}
