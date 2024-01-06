b4c1380fee63e5c92003b076957a18dad29678c1
utkarsh-gupta17 <211220061@gmail.com>
2024-01-06 21:36:23 +0530

    finalll commit

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,10 +1,18 @@
 import { NextResponse } from 'next/server'
 import jwt from "jsonwebtoken";
 import { User } from '@/models/user';
+import { connectDB } from '@/utils/db';
 
 export async function GET(request){
   const authToken = request.cookies.get("authToken")?.value;
+  if(!authToken){
+    return NextResponse.json({
+      message:"User is not Logged In",
+      success:false,
+    })
+  }
   const data = jwt.verify(authToken,process.env.JWT_KEY);
+  await connectDB();
   const user = await User.findById(data._id).select("-password");
   return NextResponse.json(user)
 }
